03ec2f1def6c30fd3427b75366181cbeabe1f2f1
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-07-11 16:45:10 +0100

    add GetAll + Save + Delete + GetBy + GetWhere

--- src/services/conditions.services.ts
+++ src/services/conditions.services.ts
@@ -4,8 +4,34 @@ const GetAll = async () =>{
     return Conditions.find()
 }
 
+const Save = async (data : any) => {
+    await Conditions.save(data)
+}
+
+const Delete = async (id : any) => {
+    await Conditions.delete(id)
+}
+
+const GetBy = async (id : any, field:any) => {
+    return await Conditions.findOne(
+        {
+            [field] : id
+        }
+    )
+}
+
+const GetWhere = async (data : any) => {
+    return await Conditions.findBy(data)
+}
+
+
+
 
 
 export {
-    GetAll
+    GetAll,
+    Save,
+    Delete,
+    GetBy,
+    GetWhere
 }
