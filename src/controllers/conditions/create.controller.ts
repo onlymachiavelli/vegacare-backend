fc03bf75567997a2092b878af18bd3e51f6f7960
Alaa Barka <tmakaveli643@gmail.com>
2023-07-11 16:58:39 +0100

    condition create controller

---
+++ src/controllers/conditions/create.controller.ts
@@ -0,0 +1,32 @@
+import {RequestHandler} from 'express'
+import Conditions from '../../models/PSchemas/conditions.schema'
+import * as Services from './../../services/conditions.services'
+import Format from 'date-and-time'
+
+
+const Create : RequestHandler =async (req, res) =>{
+    //get the request body which contains title of the condition
+    const title = req.body.title
+    if (!title) {
+        res.status(400).send("Verify you data")
+        return 
+    }
+    const condi : any = new Conditions
+    condi.title = title
+    const date : any = new Date()
+    const format : any = Format.format(
+        date,
+        'YYYY-MM-DD HH:mm:ss'
+    )
+    condi.created_at = format
+    condi.updated_at = format
+
+    await Services.Save(condi).then((resp)=>{
+        res.status(201).send("Done saving the condition")
+    }).catch((e)=>{
+        res.status(400).send("Error saving the condition")
+        console.log(e)
+    })
+}
+
+export default Create
