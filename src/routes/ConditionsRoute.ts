e1563d09e4f62f83738dbbb6dd508b7e8b0a594a
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-07-12 10:12:14 +0100

    add GetAll Conditions

--- src/routes/ConditionsRoute.ts
+++ src/routes/ConditionsRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import Create from './../controllers/conditions/create.controller'
+import GetAll from './../controllers/conditions/getAll.controller'
 
 const ConditionsRoute = Router()
 
 ConditionsRoute.post("/", Create)
+ConditionsRoute.get("/", GetAll)
 
 export default ConditionsRoute
