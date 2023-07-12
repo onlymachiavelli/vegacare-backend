337995a3465638c0bd9fff335e3bd3ca6e94300d
Alaa Barka <tmakaveli643@gmail.com>
2023-07-12 15:03:21 +0100

    pass remover

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,7 +1,7 @@
 import Users from "../models/PSchemas/user.Schema"
 
 
-const target :any =  ["id", "fullname" , "email" , "password" , "phone" , "gender" , "address", "bday","avatar" , "created_at", "updated_at" , "type" , "blood_type" , "glymecia" , "allergies", "conditons" , "medications"]
+const target :any =  ["id", "fullname" , "email"  , "phone" , "gender" , "address", "bday","avatar" , "created_at", "updated_at" , "type" , "blood_type" , "glymecia" , "allergies", "conditons" , "medications"]
 
 const Save = async (user: any) =>{
     await Users.save(user)
