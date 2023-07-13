500d88672c41e2373b73e55342b23fb2d569999a
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-07-13 15:10:13 +0100

    add socket package

--- src/controllers/users/create.controller.ts
+++ src/controllers/users/create.controller.ts
@@ -7,6 +7,10 @@ import usersModel from '../../models/MongooseSchema/users.model'
 import Format from 'date-and-time'
 import bcrypt from 'bcrypt'
 import JWT from 'jsonwebtoken'
+
+const regexMailString : string = "^.*@.*\.(a(d|e|f|g|i|l|m|n|o|r|s|t|q|u|w|x|z)|b(a|b|d|e|f|g|h|i|j|l|m|n|o|r|s|t|v|w|y|z)|c(a|c|d|f|g|h|i|k|l|m|n|o|r|u|v|x|y|z)|d(e|j|k|m|o|z)|e(c|e|g|h|r|s|t)|f(i|j|k|m|o|r)|g(a|b|d|e|f|g|h|i|l|m|n|p|q|r|s|t|u|w|y)|h(k|m|n|r|t|u)|i(d|e|q|l|m|n|o|r|s|t)|j(e|m|o|p)|k(e|g|h|i|m|n|p|r|w|y|z)|l(a|b|c|i|k|r|s|t|u|v|y)|m(a|c|d|e|f|g|h|k|l|m|n|o|q|p|r|s|t|u|v|w|x|y|z)|n(a|c|e|f|g|i|l|o|p|r|u|z)|om|p(a|e|f|g|h|k|l|m|n|r|s|t|w|y)|qa|r(e|o|s|u|w)|s(a|b|c|d|e|g|h|i|j|k|l|m|n|o|r|t|v|y|z)|t(c|d|f|g|h|j|k|l|m|n|o|r|t|v|w|z)|u(a|g|m|s|y|z)|v(a|c|e|g|i|n|u)|w(f|s)|y(e|t)|z(a|m|w))$"
+const regexMail : any = new RegExp(regexMailString)
+
 const Create : Express.RequestHandler =async (req, res) =>{
     //receive the api key 
 
@@ -15,7 +19,11 @@ const Create : Express.RequestHandler =async (req, res) =>{
     const data : any = req.body 
 
     if (!data.fullname || !data.email || !data.phone || !data.password || !data.bday ||!data.type || !data.address || !data.gender ){
-        
+        res.status(400).send("Invalid Data")
+        return 
+    }
+
+    if(!regexMail.test(data.email)){
         res.status(400).send("Invalid Data")
         return 
     }
