a45c9b522a211ba6837746127b19a6aaaa2e7bbf
Alaa Barka <tmakaveli643@gmail.com>
2023-07-20 23:42:53 +0100

    updating password

--- src/controllers/users/ResetPass.ts
+++ src/controllers/users/ResetPass.ts
@@ -1,13 +1,13 @@
 import { RequestHandler } from "express"
 import * as Services from './../../services/users.services'
 import Format from 'date-and-time'
-
+import bcrypt from 'bcrypt'
 const ResetPass :RequestHandler = async (req, res, nxt) =>{
 
     //accept code, 
     //get current date  
     //get email, and code 
-    if (!req.body.email || !req.body.code) {
+    if (!req.body.email || !req.body.code || !req.body.password) {
         res.status(400).send("email is required")
         return
     }
@@ -60,9 +60,35 @@ const ResetPass :RequestHandler = async (req, res, nxt) =>{
         res.status(401).send("Code is invalid")
         return
     }
-        res.status(200).send("Code is valid")
-        return
+
     
+    //update the password
+    const id : any = target.id
+    const saltRound: number = Number(process.env.SALT )
+    let password : any
+    password = await bcrypt
+      .genSalt(saltRound)
+      .then((s) => bcrypt.hash(req.body.password, s))
+
+    const datas : any = {
+
+        password : password,
+        code : null , 
+        codeValidity:null
+    }
+
+    try{
+        await Services.Update(id, datas)
+        console.log(`The user ${req.body.email}'s password has been updated`)
+        res.status(200).send("Password updated")
+    }
+
+    catch(e){
+        console.log(e)
+        res.status(500).send("Internal error ! ")
+        return
+    }
+
 
 
 
