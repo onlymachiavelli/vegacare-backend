49ab8a41d0dc96a261419314719f61a9c07f4b50
Alaa Barka <tmakaveli643@gmail.com>
2023-07-30 19:46:30 +0100

    blockchain req

--- src/controllers/bc/verifyToken.controller.ts
+++ src/controllers/bc/verifyToken.controller.ts
@@ -1,5 +1,36 @@
 import {RequestHandler} from 'express'
+import { ethers } from 'ethers'
+import axios from 'axios'
 
+import abi from "./abi.json"
+
+const ADDRESS :any= String(process.env.AD)
+
+async function getData(cid: string) {
+    const url = `https://gateway.pinata.cloud/ipfs/${cid}`
+    const data = await axios.get(url).then(({ data }) => data).catch(() => null)
+    return data
+}
+
+
+async function getToken(id: string) {
+    const provider = new ethers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com/")
+    const ctr = new ethers.Contract(ADDRESS, abi, provider)
+    const token = await ctr.tokenURI(id).catch(e => {
+        console.log(e)
+        return null
+    })
+
+    if (!token) return {
+        error: "There was an error while fetching the data"
+    }
+
+    const data = await getData(token)
+    if (!data) return {
+        error: "There was an error while fetching the data"
+    }
+    return data
+}
 
 const Verify : RequestHandler = async (req,res,next) => {
 
@@ -11,8 +42,8 @@ const Verify : RequestHandler = async (req,res,next) => {
     }
 
 
-    
-
+    const x = await getToken(id)
+    res.json(x)
 
 }
 
